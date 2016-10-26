895222d6ceba27c5ab908d4b24e8cd0527c85588
Giorgi Guliashvili <vinmesmiti@gmail.com>
2016-10-26 17:41:24 +0000

    process_wait changed t infinite loop, esp to -12
    process_wait changed t infinite loop, esp to -12
    
    process_wait changed t infinite loop, esp to -12

--- Project3/src/userprog/process.c
+++ Project3/src/userprog/process.c
@@ -88,7 +88,7 @@ start_process (void *file_name_)
 int
 process_wait (tid_t child_tid UNUSED) 
 {
-  return -1;
+  while(1);
 }
 
 /* Free the current process's resources. */
@@ -437,7 +437,7 @@ setup_stack (void **esp)
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
-        *esp = PHYS_BASE;
+        *esp = PHYS_BASE - 12;
       else
         palloc_free_page (kpage);
     }
